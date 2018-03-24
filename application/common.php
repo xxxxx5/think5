9c2cf6791685809ca12d5b46ae1d0e7c70c9dbce
fu <fu@outlook.com>
2018-03-24 19:28:11 +0800

    common

--- application/common.php
+++ application/common.php
@@ -1,12 +1,24 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: 流年 <liu21st@gmail.com>
-// +----------------------------------------------------------------------
+namespace app;
 
+use think\Controller;
 // 应用公共文件
+
+class common extends Controller
+{
+	function node_merge($node, $access = null,$pid=0)
+	{	
+	    $arr=array();
+	    
+	    foreach ($node as $v) {
+	    	if (is_array($access)) {
+	    		$v['access'] = in_array($v['id'],$access) ? 1 : 0;
+	    	}
+	        if ($v['pid'] == $pid) {
+	            $v['child'] = $this->node_merge($node, $access, $v['id']);
+	            $arr[]=$v;
+	        }
+	    }
+	    return $arr;
+	}
+}
